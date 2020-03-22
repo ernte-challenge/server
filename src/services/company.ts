2354d97364398a4710071987ba36c4619ad309e2
marcomaffo <marco@sheerd.de>
2020-03-22 11:12:48 +0100

    Temporär die Dateien disabled

--- src/services/company.ts
+++ src/services/company.ts
@@ -2,46 +2,46 @@ import {Company} from '../models';
 import {getRepository} from 'typeorm';
 
 export class CompanyService {
-	
-  public async getCompany(id: string): Promise<Company> {
-    const company = await getRepository(Company).findOne(companyId);    
-    return company;
-  }
-  
-  public async getAll(): Promise<Company[]> {
-    const company = await getRepository(Company).find();    
-    return company;
-  }
-  
-  public async findByName(name: string): Promise<Company[] | undefined> {
-    const company = await getRepository(Company).find({ where: { name: name } });    
-    return company;
-  }
 
-  public async create(company: Company): Promise<Company | string> {
-	if (company.name == null){
-		return "Name cannot be empty!";
-	}
-	if (company.emailAddress == null){
-		return "eMail cannot be empty!";
-	}
-	if (company.password == null){
-		return "Password cannot be empty!";
-	}
-	
-    const company = await getRepository(Company).save(company);    
-    return company;
-  }
+  // public async getCompany(id: string): Promise<Company> {
+  //   const company = await getRepository(Company).findOne(companyId);
+  //   return company;
+  // }
+	//
+  // public async getAll(): Promise<Company[]> {
+  //   const company = await getRepository(Company).find();
+  //   return company;
+  // }
+	//
+  // public async findByName(name: string): Promise<Company[] | undefined> {
+  //   const company = await getRepository(Company).find({ where: { name: name } });
+  //   return company;
+  // }
+	//
+  // public async create(company: Company): Promise<Company | string> {
+	// if (company.name == null){
+	// 	return "Name cannot be empty!";
+	// }
+	// if (company.emailAddress == null){
+	// 	return "eMail cannot be empty!";
+	// }
+	// if (company.password == null){
+	// 	return "Password cannot be empty!";
+	// }
+	//
+  //   const company = await getRepository(Company).save(company);
+  //   return company;
+  // }
+	//
+  // public async update(company: Company): Promise<Company> {
+  //   const company = await getRepository(Company).save(company);
+  //   return company;
+  // }
+	//
+  // public async delete(company: Company): Promise<Company | undefined> {
+  //   return await getRepository(Company).remove(company);
+  // }
 
-  public async update(company: Company): Promise<Company> {
-    const company = await getRepository(Company).save(company);    
-    return company;
-  }
-  
-  public async delete(company: Company): Promise<Company | undefined> {
-    return await getRepository(Company).remove(company);    
-  }
-  
 }
 
 export default new CompanyService();
--- src/services/user.ts
+++ src/services/user.ts
@@ -2,36 +2,36 @@ import {User, UserSession} from '../models';
 import {getRepository} from 'typeorm';
 
 export class UserService {
-	
-  public async getUser(id: string): Promise<User> {
-    const user = await getRepository(User).findOne(userId);    
-    return user;
-  }
 
-  public async create(user: User): Promise<User | string> {
-	if (user.name == null){
-		return "Name cannot be empty!";
-	}
-	if (user.emailAddress == null){
-		return "eMail cannot be empty";
-	}
-	if (user.password == null){
-		return "Password cannot be empty";
-	}
-	
-    const user = await getRepository(User).save(user);    
-    return user;
-  }
+  // public async getUser(id: string): Promise<User> {
+  //   const user = await getRepository(User).findOne(userId);
+  //   return user;
+  // }
+	//
+  // public async create(user: User): Promise<User | string> {
+	// if (user.name == null){
+	// 	return "Name cannot be empty!";
+	// }
+	// if (user.emailAddress == null){
+	// 	return "eMail cannot be empty";
+	// }
+	// if (user.password == null){
+	// 	return "Password cannot be empty";
+	// }
+	//
+  //   const user = await getRepository(User).save(user);
+  //   return user;
+  // }
+	//
+  // public async update(user: User): Promise<User> {
+  //   const user = await getRepository(User).save(user);
+  //   return user;
+  // }
+	//
+  // public async delete(user: User): Promise<User | undefined> {
+  //   return await getRepository(User).remove(user);
+  // }
 
-  public async update(user: User): Promise<User> {
-    const user = await getRepository(User).save(user);    
-    return user;
-  }
-  
-  public async delete(user: User): Promise<User | undefined> {
-    return await getRepository(User).remove(user);    
-  }
-  
 }
 
 export default new UserService();

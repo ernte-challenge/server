80281177260f7918bd459335cd56a95007be8c76
marcomaffo <marco@sheerd.de>
2020-03-22 17:02:31 +0100

    Einzelne Ids liefern können

--- src/controller/location.ts
+++ src/controller/location.ts
@@ -38,7 +38,7 @@ export default class LocationController implements Controller {
           ]
         },
         {
-          "id": "ds32aj3132kfk",
+          "id": "ds32aj3142kfk",
           "name": "Bauernhof Arentz",
           "whatToDoSubline": "Spargel ernten",
           "usersRegistered": 3,
@@ -68,6 +68,109 @@ export default class LocationController implements Controller {
           ]
         }
       ]);
+    });
+    httpServer.get('s/:id', async (req: Request, res: Response): Promise<any> => {
+      switch (req.params.id) {
+        case 'ds32aj3132kfk': res.send(
+          {
+            "id": "ds32aj3132kfk",
+            "name": "Bauernhof Arentz",
+            "whatToDoSubline": "Spargel ernten",
+            "usersRegistered": 3,
+            "usersNeeded": 10,
+            "distance": 20,
+            "payedPerHour": 12,
+            "bannerImageDescription": "description",
+            "locationPhoneNumber": "078612323",
+            "city": "Kölle",
+            "zipCode": "50623",
+            "houseNumber": "19A",
+            "street": "Lange Straße",
+            "bannerImageSrc": "https://assets.t3n.sc/news/wp-content/uploads/2019/10/microsoft-agrartech-grand-farm.jpg?auto=format&fit=crop&h=348&ixlib=php-2.3.0&w=620",
+            "mainAreasOfActivity": [
+              "Mithilfe bei der Ernte von Gemüse und Obst",
+              "Mithilfe bei der Befüllung",
+              "Arbeiten im Freien"
+            ],
+            "searchedProfile": [
+              "Körperliche Fitness",
+              "Zuverlässigkeit",
+              "Freude an der frischen Luft zu arbeiten"
+            ],
+            "whatYouGet": [
+              "Frische Luft",
+              "Unendliche Dankbarkeit für ihr Engagement"
+            ]
+          }
+        );
+        break;
+        case 'ds32aj3142kfk': res.send(
+          {
+            "id": "ds32aj3142kfk",
+            "name": "Bauernhof Arentz",
+            "whatToDoSubline": "Spargel ernten",
+            "usersRegistered": 3,
+            "usersNeeded": 10,
+            "distance": 20,
+            "payedPerHour": 12,
+            "bannerImageDescription": "description",
+            "locationPhoneNumber": "078612323",
+            "city": "Kölle",
+            "zipCode": "50623",
+            "houseNumber": "19A",
+            "street": "Lange Straße",
+            "bannerImageSrc": "https://assets.t3n.sc/news/wp-content/uploads/2019/10/microsoft-agrartech-grand-farm.jpg?auto=format&fit=crop&h=348&ixlib=php-2.3.0&w=620",
+            "mainAreasOfActivity": [
+              "Mithilfe bei der Ernte von Gemüse und Obst",
+              "Mithilfe bei der Befüllung",
+              "Arbeiten im Freien"
+            ],
+            "searchedProfile": [
+              "Körperliche Fitness",
+              "Zuverlässigkeit",
+              "Freude an der frischen Luft zu arbeiten"
+            ],
+            "whatYouGet": [
+              "Frische Luft",
+              "Unendliche Dankbarkeit für ihr Engagement"
+            ]
+          }
+        );
+        break;
+        default:
+        res.send(
+          {
+            "id": "ds32aj3142kfk",
+            "name": "Bauernhof Arentz",
+            "whatToDoSubline": "Spargel ernten",
+            "usersRegistered": 3,
+            "usersNeeded": 10,
+            "distance": 20,
+            "payedPerHour": 12,
+            "bannerImageDescription": "description",
+            "locationPhoneNumber": "078612323",
+            "city": "Kölle",
+            "zipCode": "50623",
+            "houseNumber": "19A",
+            "street": "Lange Straße",
+            "bannerImageSrc": "https://assets.t3n.sc/news/wp-content/uploads/2019/10/microsoft-agrartech-grand-farm.jpg?auto=format&fit=crop&h=348&ixlib=php-2.3.0&w=620",
+            "mainAreasOfActivity": [
+              "Mithilfe bei der Ernte von Gemüse und Obst",
+              "Mithilfe bei der Befüllung",
+              "Arbeiten im Freien"
+            ],
+            "searchedProfile": [
+              "Körperliche Fitness",
+              "Zuverlässigkeit",
+              "Freude an der frischen Luft zu arbeiten"
+            ],
+            "whatYouGet": [
+              "Frische Luft",
+              "Unendliche Dankbarkeit für ihr Engagement"
+            ]
+          }
+        );
+      }
     });
   }
 }
